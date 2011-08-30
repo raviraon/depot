64c84d1f936b1285dbe6957e647abe9317f5d14c
Ravi <slicehost@thoughtworks.com>
2011-08-30 19:11:21 +0530

    basic product model setup with validation tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :price , :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => "Image name should end wih gt or png or jpg"
+  }
 end
